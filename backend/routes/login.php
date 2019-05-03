2b2cdb04427a16ea00c93387f0ad7c1700732be1
Vitor <vitorzang@yahoo.com.br>
2019-05-03 13:11:04 -0400

    Update login.php

--- backend/routes/login.php
+++ backend/routes/login.php
@@ -6,8 +6,8 @@ $app->post('/api/login', function ($request, $response, $args) { //POST example
     $password = $params['password'];
 
     $selectStatement = $pdo->select()
-                            ->from('users')
-                            ->where('email', '=', $email)
+                            ->from('login')
+                            ->where('username', '=', $username)
                             ->where('password', '=', $password);
 
 	$stmt = $selectStatement->execute();
@@ -23,33 +23,4 @@ $app->post('/api/login', function ($request, $response, $args) { //POST example
 	$pdo = null;
 	return $response;
 });
-
-//
-// demo of update
-$app->post('/api/chgpwd', function ($request, $response, $args) { //POST example
-    $pdo =$this->pdo;
-    $params = $request->getParsedBody();
-	$email = $params['email'];
-    $password = $params['password'];
-    $new_password = $params['new_password'];
-
-    $updateStatement = $pdo->update(array("password" => $new_password))
-                            ->table('users')
-                            ->where('email', '=', $email)
-                            ->where('password', '=', $password);
-
-	$stmt = $updateStatement->execute();
-    
-
-    if ($stmt > 0) // 1 row affected, success
-        $res['status'] = true;
-    else
-        $res['status'] = false;
-
-    $response->write(json_encode($res));
-    
-	$pdo = null;
-	return $response;
-});
-
 ?>
